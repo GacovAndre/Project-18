76771d8cf88140e7ede729e4d60219c9d445ba7d
GacovAndre <gacovandre79@gmail.com>
2023-12-04 18:31:32 +0100

    Project18

---
+++ src/Sobiranje.java
@@ -0,0 +1,9 @@
+public class Sobiranje {
+public static void main(String[] args) {
+int sum = Soberi_broevi(2, 3, 4);
+System.out.println("Sumata na trite broja iznesuva: " + sum);
+}
+public static int Soberi_broevi(int a, int b, int c){
+return (a+b+c);
+}
+}
